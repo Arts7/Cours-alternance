4bd121eec2bbb2fda76472e31d4a313f12575074
Arthur Jouanneau <jouanneauarthur@gmail.com>
2020-09-23 15:16:04 +0200

    Exo heritage part 1

---
+++ part2_classes/Exercices/heritage.js
@@ -0,0 +1,36 @@
+class Rectangle {
+
+    constructor(h, w) {
+      this._h = h;
+      this._w = w;
+    }
+  
+    perimetre() {
+        return (this._w + this._h) * 2;
+    }
+
+    area(){
+        return this._w * this._h;
+    }
+
+    set w(w){
+        this._w = w;
+    }
+
+    set h(h){
+        this._h = h;
+    }
+}
+  
+class Square extends Rectangle {
+    constructor(l) {
+        super(l, l);
+    }
+}
+
+let square = new Square(8, 9);
+square.w = 7;
+
+console.log(square.area());
+console.log(square);
+
