af472184e5df7ae2fa9e01a71038e8ea7691dbb6
Arthur Jouanneau <jouanneauarthur@gmail.com>
2020-09-23 17:29:54 +0200

    dragon V2 with setter

--- part2_classes/Exercices/dragon-knight-V2.js
+++ part2_classes/Exercices/dragon-knight-V2.js
@@ -10,8 +10,6 @@ class Game {
             randomShoot == 0 ? this._dragon.life = this._dragon.life - this._knight.force * this._knight.multiShot : this._knight.life = this._knight.life - this._dragon.force * this._dragon.multiShot;
             randomShoot == 0 ? console.log(this._knight._name + ` inglige ` + this._knight.force * this._knight.multiShot) : console.log(this._dragon._name + ` inglige ` + this._knight.force * this._knight.multiShot);
             console.log('Vie restante de '+this._knight._name + ' : ' + this._knight.life + '\n' + 'Vie restante de '+this._dragon._name + ' : ' + this._dragon.life);
-
-            time()
         }
         return this._dragon.life <= 0 ? `Pour le Norfendre ` + this._knight._name + ` a gagné !!` : 'Wall of fire ' + this._dragon._name + ` a gagné !!`;
     }
@@ -58,11 +56,6 @@ class Knight extends Player {
     }
 }
 
-function time() { 
-    setTimeout(function() { 
-        console.log('adzdazd');
-    }, 5000); 
-} 
 function randomNumber (number) {
     return Math.round(Math.random() * number);
 }
