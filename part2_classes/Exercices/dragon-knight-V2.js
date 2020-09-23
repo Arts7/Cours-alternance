098897422edaeb17dd9e108eac6b7182085f1018
Arthur Jouanneau <jouanneauarthur@gmail.com>
2020-09-23 17:26:24 +0200

    dragon V2 with setter

--- part2_classes/Exercices/dragon-knight-V2.js
+++ part2_classes/Exercices/dragon-knight-V2.js
@@ -5,16 +5,15 @@ class Game {
     }
 
     run () {
-        let life_dragon =  this._dragon.life;
-        let life_knight =  this._knight.life;
-        console.log(life_dragon, life_knight);
-        while (life_knight > 0 && life_dragon  > 0) {
+        while (this._dragon.life > 0 && this._knight.life  > 0) {
             let randomShoot = Math.round(Math.random() * 1);
-            randomShoot == 0 ? life_dragon = life_dragon - this._knight.force * this._knight.multiShot : life_knight = life_knight - this._dragon.force * this._dragon.multiShot;
+            randomShoot == 0 ? this._dragon.life = this._dragon.life - this._knight.force * this._knight.multiShot : this._knight.life = this._knight.life - this._dragon.force * this._dragon.multiShot;
             randomShoot == 0 ? console.log(this._knight._name + ` inglige ` + this._knight.force * this._knight.multiShot) : console.log(this._dragon._name + ` inglige ` + this._knight.force * this._knight.multiShot);
-            console.log('Vie restante de '+this._knight._name + ' : ' + life_knight + '\n' + 'Vie restante de '+this._dragon._name + ' : ' + life_dragon);
+            console.log('Vie restante de '+this._knight._name + ' : ' + this._knight.life + '\n' + 'Vie restante de '+this._dragon._name + ' : ' + this._dragon.life);
+
+            time()
         }
-        return life_dragon <= 0 ? `Pour le Norfendre ` + this._knight._name + ` a gagné !!` : 'Wall of fire ' + this._dragon._name + ` a gagné !!`;
+        return this._dragon.life <= 0 ? `Pour le Norfendre ` + this._knight._name + ` a gagné !!` : 'Wall of fire ' + this._dragon._name + ` a gagné !!`;
     }
 }
 
@@ -25,6 +24,8 @@ class Player {
         this._multiShot = multiShot;
         this._name      = name;
     }
+
+    // Getters
     get force() {
         return randomNumber(this._force);
     }
@@ -36,6 +37,11 @@ class Player {
     }
     get name() {
         this._name;
+    }
+
+    //Setter
+    set life(life) {
+        this._life = life;
     }
 }
 
@@ -52,7 +58,11 @@ class Knight extends Player {
     }
 }
 
-
+function time() { 
+    setTimeout(function() { 
+        console.log('adzdazd');
+    }, 5000); 
+} 
 function randomNumber (number) {
     return Math.round(Math.random() * number);
 }
